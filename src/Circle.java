c4fc5d894f32c975cd3df7f0faa949acb534f3f3
Darya Simerova <darya.simerova@gmail.com>
2018-06-02 21:55:38 +0300

    Not working

--- src/Circle.java
+++ src/Circle.java
@@ -11,19 +11,53 @@ class Circle extends Figures {
     public Circle(Color color, double lineWidth, Point center, double radius) {
         this(color, lineWidth, center);
         this.radius = radius;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
-    public void setRadius(double radius) {
-        this.radius = radius;
+        photoName ="circle.png";
     }
 
     @Override
-    void draw(GraphicsContext gc) {
+    public void printFigure() {
+        System.out.println("  * ^ * ");
+        System.out.println(" *     *");
+        System.out.println("  * _ *");
+    }
+
+    @Override
+    public void calculateAndShowSquare() {
+
+        square = (int)(3.14*radius*radius);
+        System.out.println("square of Circle = " + square + " mm2");
+    }
+
+    @Override
+    public void showFormulaOfSquare() {
+        System.out.println("Square of Circle = 3.14*radius^2.");
+    }
+
+    @Override
+    public String getInformation() {
+        String str =
+//        "  * ^ * " + "\n"+
+//        " *     *" + "\n"+
+//        "  * _ * " + "\n"+
+                "Input data : Radius = " + radius + "\n"+
+                        "square of Circle = " + square + " mm2" + "\n"+
+                        "Square of Circle = 3.14*radius^2." + "\n";
+        return str;
+    }
+
+    @Override
+    public void draw(GraphicsContext graphicsContext) {
+        graphicsContext.setLineWidth(lineWidth);
+        graphicsContext.setStroke(color);
+        graphicsContext.strokeOval(center.x- radius, center.y - radius, radius*2, radius*2);
+
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
     }
 }
-
-
--- src/controller/MainScreenController.java
+++ src/controller/MainScreenController.java
@@ -1,22 +1,109 @@
 package controller;
 
+import com.company.java20180520.figures.*;
+import com.sun.org.apache.xml.internal.security.signature.reference.ReferenceNodeSetData;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
+import java.awt.*;
+import java.awt.image.PixelInterleavedSampleModel;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class MainScreenController implements Initializable {
-    public Canvas canvas;
+    @FXML
+    private Canvas canvas;
+    private Figure [] figures;
+    private Random rmd;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        figures = new Figure[1];
+        rmd = new Random( System.currentTimeMillis() );
+    }
+
+    private Figure createFigure (Point point){
+
+        Figure figure = null;
+
+        switch (rmd.nextInt(3)){
+
+            case Figure.FIGURE_TYPE_RECTANCLE:
+                figure = new Rectangle( rmd.nextInt(5),
+                        Color.CORNFLOWERBLUE,
+                        point,
+                        rmd.nextInt(100),
+                        rmd.nextInt(100) );
+                break;
+
+            case Figure.FIGURE_TYPE_TRIANGLE:
+                figure = new Triangle(rmd.nextInt(5),
+                        Color.DARKCYAN,
+                        point,
+                        rmd.nextInt(100));
+                break;
+
+            case Figure.FIGURE_TYPE_CIRCLE:
+                figure = new Circle(rmd.nextInt(5),Color.RED, point, rmd.nextInt(100));
+                break;
+
+            default:
+                System.out.println("Unknown Figure type !!!");
+        }
+
+
+        return figure;
+    }
+
+    private  void addFigure (Figure figure){
+
+        if(figures==null) return;
+        if(figures[figures.length-1] == null){
+            figures[figures.length-1] = figure;
+        }else{
+            Figure [] tmp =  new Figure[figures.length + 1];
+            int index = 0;
+            for( ; index<figures.length; index++){
+                tmp[index] = figures[index];
+            }
+            tmp[index] = figure;
+            figures = tmp;
+        }
+
 
     }
 
-    public void canvasClicked(MouseEvent mouseEvent) {
-        System.out.println("X = " + mouseEvent.getX());
-        System.out.println("Y = " + mouseEvent.getY());
+
+    private  void rePaint (){
+
+        canvas.getGraphicsContext2D().clearRect(0,0, 1024,600);
+        for (Figure figure : figures) {
+            if(figure != null){
+                figure.draw(canvas.getGraphicsContext2D());
+            }
+        }
+
+
+
     }
+
+    @FXML
+    private void canvasClicked(MouseEvent event) {
+        System.out.println("X = " + event.getX() + ", Y = " + event.getY() );
+        Point point = new Point( event.getX(), event.getY() );
+     Figure figure  = createFigure(point);
+       addFigure(figure);
+        rePaint();
+
+
+    }
+
+
+
+
+
 }
